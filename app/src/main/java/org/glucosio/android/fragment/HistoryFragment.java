6e73dcee252511fb1da5e2328be92af9420f6eb1
Mark Archibald <mark030295@gmail.com>
2016-12-13 15:18:01 +0000

    Changed fadeout speed when blood level item deleted
    Lowered the fadeout speed of blood level item deletion from 2000ms to
    200ms.

--- app/src/main/java/org/glucosio/android/fragment/HistoryFragment.java
+++ app/src/main/java/org/glucosio/android/fragment/HistoryFragment.java
@@ -216,7 +216,7 @@ public class HistoryFragment extends Fragment {
                 TextView idTextView = (TextView) itemView.findViewById(R.id.item_history_id);
                 final long idToDelete = Long.parseLong(idTextView.getText().toString());
                 final CardView item = (CardView) itemView.findViewById(R.id.item_history);
-                item.animate().alpha(0.0f).setDuration(2000);
+                item.animate().alpha(0.0f).setDuration(200);
                 Snackbar.make(((MainActivity) getActivity()).getFabView(), R.string.fragment_history_snackbar_text, Snackbar.LENGTH_SHORT).setCallback(new Snackbar.Callback() {
                     @Override
                     public void onDismissed(Snackbar snackbar, int event) {
